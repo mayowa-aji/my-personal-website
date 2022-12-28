7f88f0371a3bb748b29670083929c320e4728905
Mayowa Ajiboye <mayaajiboye@gmail.com>
2022-12-28 16:17:27 -0500

    change h2 text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "is my javascript still working ";
 
 document.querySelector("body").appendChild(h2);
